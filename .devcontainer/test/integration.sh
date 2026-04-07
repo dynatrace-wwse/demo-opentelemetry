10bfadb622e1a873dee83de8e07168e0945be70f
Sergio Hinojosa <hj.sergio@gmail.com>
2026-04-07 16:53:45 +0000

    adding wait
    Signed-off-by: Sergio Hinojosa <hj.sergio@gmail.com>

--- .devcontainer/test/integration.sh
+++ .devcontainer/test/integration.sh
@@ -4,15 +4,12 @@ source .devcontainer/util/source_framework.sh
 
 printInfoSection "Running integration Tests for $RepositoryName"
 
-#TODO: In here you add your assertions
-#assertRunningPod dynatrace operator
-
-#assertRunningPod dynatrace activegate
-
-#assertRunningPod dynatrace oneagent
-
 assertRunningPod opentelemetry-demo ad
 
 assertRunningPod opentelemetry-demo frontend
+
+printInfo "waiting for the app to start fully since we are not adding a wait in the post-start"
+
+waitForAllReadyPods opentelemetry-demo 
 
 assertRunningApp 30100
